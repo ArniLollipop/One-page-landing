68cbae9b70472b394becd2a91fefff481b51bce4
ArniLollipop <bocunahero@gmail.com>
2022-12-01 20:09:07 +0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
     <div className='text-center py-10 px-4'>
       <h2 className="md:mb-10 sm:mb-8 mb-6">Contacts</h2>
       <p className=" my-6">Want to know more or just chat? <br/> You are welcome!</p>
-      <button className='bg-black rounded-full text-white sm:py-3 py-1 md:px-8 w-full text-lg'>Send message</button>
+      <button className='bg-black rounded-full text-white sm:py-3 py-1 sm:px-8 sm:w-fit w-full text-lg'>Send message</button>
       <div className='py-10 w-fit mx-auto block'>
         <div className='flex items-center justify-between gap-x-10'>
           <img src={LinkedIn} alt="qwe"/>
